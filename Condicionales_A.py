acc8bb21f05b487f2ba89fa28f0a8f06a8ee8684
David Ruiz <52647210+davinciruiz@users.noreply.github.com>
2021-07-03 04:56:16 -0500

    Add files via upload

---
+++ Condicionales_A.py
@@ -0,0 +1,27 @@
+#David Ruiz 
+#Condicionales
+
+#Creamos una afirmación el tipo de clima de hoy
+
+hoyClima = 'frio'
+
+
+#checa si el clima de hoy es igual a lluvioso
+
+if hoyClima == 'lluvioso':
+    print('abrigolluvia') 
+    print('botaslluvia')
+
+#si no es asi entonces checa el clima de hoy y pongamos que es igual a clima 'frio' 
+
+elif hoyClima == 'frio':
+    print('playeraconmangas') 
+    print('bufanda') 
+
+#si el clima no es igual a ''lluvioso'' entonces haz lo siguiente
+
+else: 
+    print('playera lacoste')
+    print('shorcitos')
+
+print('listo') 
